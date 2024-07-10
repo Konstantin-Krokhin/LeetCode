1b84939b599e88113e6f2a7cdcefe24ae91a4ce3
Konstantin Krokhin <kokrokhi@students.42wolfsburg.de>
2024-07-10 17:22:12 -0400

    ValidParentheses_FullSolution
    Full solution of ValidParentheses

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -3,56 +3,64 @@ public:
     bool isValid(string s)
     {
         int brac1 = 0, brac2 = 0, brac3 = 0, i = 0;
-        int flag1 = 0, flag2 = 0, flag3 = 0;
+        int flag1 = 0, flag2 = 0, flag3 = 0, k = 0;
         char active;
+        char prevbrac[20000];
 
         while (s[i] != '\0')
         {
-            printf("%i, %i, %i, %i\n", i, flag1, flag2, flag3);
             if (s[i] == '(')
             {
                 brac1++;
                 active = '(';
+                prevbrac[k] = '(';
+                k++;
                 flag1 = 1;
             }
             else if (s[i] == ')')
             {
-                
-                if (active == '(' || brac1 > 0) flag1 = 0;
+                if (k > 0) k--;
+                if (active == '(' || (brac1 > 0 && '(' == prevbrac[k])) flag1 = 0;
                 else return false;
                 brac1--;
+                active = ' ';
             }
             else if (s[i] == '[')
             {
                 brac2++;
                 active = '[';
+                prevbrac[k] = '[';
+                k++;
                 flag2 = 1;
             }
             else if (s[i] == ']')
             {
-                
-                if (active == '[' || brac2 > 0) flag2 = 0;
+                if (k > 0) k--;
+                if (active == '[' || (brac2 > 0 && '[' == prevbrac[k])) flag2 = 0;
                 else return false;
                 brac2--;
+                active = ' ';
             }
             else if (s[i] == '{')
             {
                 brac3++;
                 active = '{';
+                prevbrac[k] = '{';
+                k++;
                 flag3 = 1;
             }
             else if (s[i] == '}')
             {
-                
-                if (active == '{' || brac3 > 0) flag3 = 0;
+                if (k > 0) k--;
+                if (active == '{' || (brac3 > 0 && '{' == prevbrac[k])) flag3 = 0;
                 else return false;
                 brac3--;
+                active = ' ';
             }
             
             i++;
         }
-        printf("%i, %i, %i, %i\n", i, brac1, brac2, brac3);
-        if (brac1 == brac2 == brac3 == 0)
+        if (brac1 == 0 && brac2 == 0 && brac3 == 0)
             return true;
         else
             return false;
