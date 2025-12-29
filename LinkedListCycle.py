ff698ef5cb10fdedc2f53ac9ba2626a72ccf14f3
Konstantin Krokhin <konstakrokhin@gmail.com>
2025-12-28 23:18:36 -0500

    Linked List Cycle Better Pythonic solution

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -6,15 +6,15 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return False
-
         slow = head
-        fast = head.next
-
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
+        fast = head
+        
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return True
+
+            if slow == fast:
+                return True
+        
+        return False
+            
